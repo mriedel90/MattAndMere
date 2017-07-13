a500fe8802d64940fc9db623215179585e15488e
Matt Riedel <Matt.Riedel@ascensus.com>
2017-07-13 14:51:30 -0400

    removed email

--- Source/MattAndMere/Entities/Reservation.cs
+++ Source/MattAndMere/Entities/Reservation.cs
@@ -10,7 +10,6 @@ namespace MattAndMere.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Email { get; set; }
         public bool WillAttend { get; set; }
         public MealChoice MealChoice { get; set; }
         public bool Shuttle { get; set; }
